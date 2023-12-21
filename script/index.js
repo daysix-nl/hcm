31ca6b37864369fb1c116e464d40209f0572388b
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2023-12-21 09:42:27 +0100

    wip

--- script/index.js
+++
@@ -1,9 +0,0 @@
-try {
-  const button = document.querySelector(".button-hamburger");
-
-  button.addEventListener("click", () => {
-    const menu = document.querySelector(".menu");
-    button.classList.toggle("button-hamburger--active");
-    menu.classList.toggle("menu--active");
-  });
-} catch (error) {}
